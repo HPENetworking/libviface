2272f7e3ca6b7612d4d73306e6bd5e7f30f652bb
Carlos Miguel Jenkins Perez <carlos.jenkins@hp.com>
2015-07-13 20:29:25 -0600

    fix: dev: Added missing documentation to some utility functions.

--- include/viface/utils.hpp
+++ include/viface/utils.hpp
@@ -28,14 +28,36 @@
 
 namespace viface
 {
+namespace utils
+{
+
 /**
  * @ingroup libviface Public Interface
  * @{
  */
 
+/**
+ * Parse a MAC address in string format.
+ *
+ * @param[in]  mac MAC address to parse in format "56:84:7a:fe:97:99".
+ *
+ * @return A vector of 6 bytes with the parsed MAC address.
+ *         If the string cannot be parsed an invalid_argument exception is
+ *         thrown.
+ */
 std::vector<uint8_t> parse_mac(std::string const& mac);
+
+/**
+ * Build a hexdump representation of a binary blob.
+ *
+ * @param[in]  bytes Binary blob (array of bytes) with the message to
+ *             build represention.
+ *
+ * @return A string with the hexdump representation of the input binary blob.
+ */
 std::string hexdump(std::vector<uint8_t> const& bytes);
 
 /** @} */ // End of libviface
+};
 };
 #endif // _VIFACE_UTILS_HPP
